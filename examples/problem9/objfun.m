f9a433a4d72289d35912a39ee0cdc0a93ae06c22
Martin Jelemensky <martin.jelemensky@stuba.sk>
2017-04-05 10:23:39 +0200

    moved objfun,confun,process files from examples into dynoptim main directory -- still need to rewrite the files

--- examples/problem9/objfun.m
+++
@@ -1,9 +0,0 @@
-function [f,Df] = objfun(t,x,u,p)
-% objective function
-f = x(2); % J
-
-% gradients of the objective function
-Df.t = [];      % dJ/dt
-Df.x = [0;1;0]; % dJ/dx
-Df.u = [];      % dJ/du
-Df.p = [];      % dJ/dp
